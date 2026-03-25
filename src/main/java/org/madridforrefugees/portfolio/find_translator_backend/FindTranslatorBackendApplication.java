31e2cc8eef471c6dd393529e79e2efc3abcb3850
Hao Tri Pham <phamhaotri@googlemail.com>
2026-03-25 11:14:36 +0100

    Spring Initializr project

---
+++ src/main/java/org/madridforrefugees/portfolio/find_translator_backend/FindTranslatorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.madridforrefugees.portfolio.find_translator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTranslatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTranslatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/madridforrefugees/portfolio/find_translator_backend/FindTranslatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.madridforrefugees.portfolio.find_translator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindTranslatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
